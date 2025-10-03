3a1b99dbd606b09456d453bc9fe1d7bb5d05729c
Hunter <hunterweisenbach@me.com>
2025-10-03 19:48:51 -0400

    start and stop buttons added

--- timer.js
+++ timer.js
@@ -1,13 +1,19 @@
 const button = document.querySelector('button');
 let seconds = 0;
 let timer = null;
-
+const secondsDisplay = document.querySelector('.seconds h1');
 
 function onClick() {
     if (!timer) {
         timer = setInterval(() => {
             seconds++;
-            console.log(seconds);
+            console.log(seconds); // DEBUGGING PURPOSES
         }, 1000);
     }
+}
+
+function stop() {
+    clearInterval(timer);
+    timer = null;
+    console.log(`Timer stopped at ${seconds} seconds`); //DEBUGGING PURPOSES
 }
